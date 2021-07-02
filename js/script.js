f1d6bd8655f7455cda929df468ffe9ecde43c2ec
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-07-02 10:00:53 +0200

    dfg

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 const cardCont = document.querySelectorAll(".card-cont aside");
 const tabBtn = document.querySelectorAll(".tab-btn");
 
-
 tabBtn.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const clicked = e.currentTarget.dataset.id;
-    
   });
 });
-
-
-
